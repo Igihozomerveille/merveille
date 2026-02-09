9e810d98a63655ba10698ac965c629e23afc5826
Igihozomerveille <ghzgihozo@gmail.com>
2026-02-09 15:17:03 +0000

    Add files via upload

---
+++ mmmm.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ok</title>
+</head>
+<a href="wao.php">login</a>
+    <a href="register.php">register</a>
+<body>
+    <fieldset>
+        <legend>wel come to my web</legend>
+        <form action="insert.php" method='POST'>
+            <label for="">user name</label>
+            <input type="text"name="username"><br><br>
+            <label for="">password</label>
+            <input type="text" name="pass" id=""><br><br>
+            <label for="">location</label>
+            <input type="text" name="location" id=""><br><br>
+            <input type="submit" value="login">
+            <input type="submit" value="register">
+        </form>
+    </fieldset>
+    
+</body>
+</html>
